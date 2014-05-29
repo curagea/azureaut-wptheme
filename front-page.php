11004f3381cde8b557ba405b502a7cdcc838844a
Hannah Hu <hannahlhu@gmail.com>
2014-05-29 15:49:28 -0700

    spaces to tabs. sorry

--- front-page.php
+++ front-page.php
@@ -1,30 +1,30 @@
 <?php
 /* Template Name: Frontpage */
-  get_header();
+	get_header();
 ?>
 
 <div class="frontpage">
-  <div class="welcome-image">
-    <img src="<? echo get_template_directory_uri() ?>/images/front-page.png" alt="">
-  </div>
-  <h1>Oh hi!</h1>
-  <p class="intro mar-bot_3">
-    I&rsquo;m Hannah, and welcome to my site. It has a name, &ldquo;Azure
-    Autonomie&rdquo;, for reasons. Feel free to explore and discover here, and
-    who knows - you might learn a thing or two <a href="/about">about me</a>.
-  </p>
+	<div class="welcome-image">
+		<img src="<? echo get_template_directory_uri() ?>/images/front-page.png" alt="">
+	</div>
+	<h1>Oh hi!</h1>
+	<p class="intro mar-bot_3">
+		I&rsquo;m Hannah, and welcome to my site. It has a name, &ldquo;Azure
+		Autonomie&rdquo;, for reasons. Feel free to explore and discover here, and
+		who knows - you might learn a thing or two <a href="/about">about me</a>.
+	</p>
 
-  <nav>
-    <ul class="grid-parent">
-      <li><a href="<?php echo page_link('journal') ?>" class="journal"><span>Journal</span></a></li>
-      <li><a href="<?php echo page_link('pastime') ?>" class="pastime"><span>Pastime</span></a></li>
-      <li><a href="<?php echo page_link('portfolio') ?>" class="portfolio"><span>Portfolio</span></a></li>
-      <li><a href="<?php echo page_link('about') ?>" class="about"><span>About</span></a></li>
-    </ul>
-  </nav>
-  <footer class="grid-100">
-    <?php get_template_part('copyright') ?>
-  </footer>
+	<nav>
+		<ul class="grid-parent">
+			<li><a href="<?php echo page_link('journal') ?>" class="journal"><span>Journal</span></a></li>
+			<li><a href="<?php echo page_link('pastime') ?>" class="pastime"><span>Pastime</span></a></li>
+			<li><a href="<?php echo page_link('portfolio') ?>" class="portfolio"><span>Portfolio</span></a></li>
+			<li><a href="<?php echo page_link('about') ?>" class="about"><span>About</span></a></li>
+		</ul>
+	</nav>
+	<footer class="grid-100">
+		<?php get_template_part('copyright') ?>
+	</footer>
 
 </div> <!-- end .frontpage -->
 
--- header.php
+++ header.php
@@ -2,18 +2,18 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-  <meta charset="<?php bloginfo('charset'); ?>" />
-  <link rel="icon" href="/favicon.gif" type="image/x-icon" />
-  <link rel="shortcut icon" href="/favicon.gif" type="image/x-icon" />
-  <title><?php wp_title( '|', true, 'right' ); bloginfo( 'name' ); ?></title>
+	<meta charset="<?php bloginfo('charset'); ?>" />
+	<link rel="icon" href="/favicon.gif" type="image/x-icon" />
+	<link rel="shortcut icon" href="/favicon.gif" type="image/x-icon" />
+	<title><?php wp_title( '|', true, 'right' ); bloginfo( 'name' ); ?></title>
 
-  <? if (is_front_page()): ?>
-    <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri() ?>/css/frontpage.css">
-  <? else: ?>
-    <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri() ?>/css/main.css">
-  <? endif; ?>
-  <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-  <?php wp_head(); ?>
+	<? if (is_front_page()): ?>
+		<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri() ?>/css/frontpage.css">
+	<? else: ?>
+		<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri() ?>/css/main.css">
+	<? endif; ?>
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class() ?>>
