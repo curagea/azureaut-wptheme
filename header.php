158a6bb34f2021fec872d5cac5632a896913248e
Hannah Hu <hannahlhu@gmail.com>
2014-05-29 15:33:51 -0700

    move style.css around

--- header.php
+++ header.php
@@ -5,11 +5,12 @@
   <meta charset="<?php bloginfo('charset'); ?>" />
   <link rel="icon" href="/favicon.gif" type="image/x-icon" />
   <link rel="shortcut icon" href="/favicon.gif" type="image/x-icon" />
-  <title><?php wp_title(); ?></title>
+  <title><?php wp_title( '|', true, 'right' ); bloginfo( 'name' ); ?></title>
 
-  <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>">
   <? if (is_front_page()): ?>
     <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri() ?>/css/frontpage.css">
+  <? else: ?>
+    <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri() ?>/css/main.css">
   <? endif; ?>
   <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
   <?php wp_head(); ?>
