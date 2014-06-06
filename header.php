79fd81b9ca3225497ffb2a821ee2aff8cbbdd931
Hannah Hu <hannahlhu@gmail.com>
2014-06-05 19:18:12 -0700

    synchronous web fonts

--- header.php
+++ header.php
@@ -2,20 +2,14 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-	<script type="text/javascript">
-		WebFontConfig = {
-			google: { families: [ 'Ek+Mukta:700:latin', 'Cardo::latin' ] }
-		};
-		(function() {
-			var wf = document.createElement('script');
-			wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-				'://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-			wf.type = 'text/javascript';
-			wf.async = 'true';
-			var s = document.getElementsByTagName('script')[0];
-			s.parentNode.insertBefore(wf, s);
-		})();
-	</script>
+<script src="//ajax.googleapis.com/ajax/libs/webfont/1/webfont.js"></script>
+<script>
+  WebFont.load({
+    google: {
+      families: ['Ek+Mukta:700:latin', 'Cardo::latin']
+    }
+  });
+</script>
 
 	<meta charset="<?php bloginfo('charset'); ?>" />
 	<link rel="icon" href="/favicon.gif" type="image/x-icon" />
