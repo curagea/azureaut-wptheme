72ce4c529c85ccba1a7908a78815ef4e41748c38
Hannah Hu <hannahlhu@gmail.com>
2014-06-06 13:11:10 -0700

    fuck me journal

--- portfolio.php
+++ portfolio.php
@@ -1,29 +1,29 @@
 <?php
-/* Template Name: Portfolio */
+	/* Template Name: Portfolio */
 	get_header();
 ?>
 
-	<h1>Portfolio</h1>
-	<?php
-		query_posts('category_name=portfolio');
-		if (have_posts()):
-			while (have_posts()) : the_post();
-	?>
-		<div class="chunk" id="post-<?php the_ID(); ?>">
-			<? echo get_image('thumbnail'); ?>
-			<h3><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h3>
-			<span class="meta date">
-				<?= get('project-start') . ' &mdash; ' . get('project-end'); ?></span>
-			<span class="meta tags"><a href="<?= get('project-link') ?>"><?= get('project-link') ?></a></span>
-			<p class="description"><?php the_excerpt(); ?></p>
-			<a class="more-link" href="<?php the_permalink() ?>">More about this project</a>
-		</div>
+<h1>Portfolio</h1>
+<?php
+	query_posts('category_name=portfolio');
+	if (have_posts()):
+		while (have_posts()) : the_post();
+?>
+	<div class="chunk" id="post-<?php the_ID(); ?>">
+		<h3><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h3>
+		<? echo get_image('thumbnail'); ?>
+		<span class="meta date">
+			<?= get('project-start') . ' &mdash; ' . get('project-end'); ?></span>
+		<span class="meta tags"><a href="<?= get('project-link') ?>"><?= get('project-link') ?></a></span>
+		<div class="description"><?php the_excerpt(); ?></div>
+		<a class="more-link" href="<?php the_permalink() ?>">More about this project</a>
+	</div>
 
-	<?php 
-		endwhile;
-		else:
-	?>
-		<p>Patience is a virtue...</p>
-	<? endif; ?>
+<?php
+	endwhile;
+	else:
+?>
+	<p>Patience is a virtue...</p>
+<?php endif; ?>
 
 <?php get_footer(); ?>
