909353edc1c07197b9b8981734e70b9d58ae6800
Hannah Hu <hannahlhu@gmail.com>
2014-07-01 16:01:43 -0700

    better accessibility for external links?

--- external-links.php
+++ external-links.php
@@ -3,12 +3,34 @@
 ?>
 
 <div class="external-links">
-	<a href="#" class="fb"><img class="sprite" alt="" src="<?php echo $external_icons_img; ?>"><span>Facebook</span></a>
-	<a href="#" class="twitter"><img class="sprite" alt="" src="<?php echo $external_icons_img; ?>"><span>Twitter</span></a>
-	<a href="#" class="flickr"><img class="sprite" alt="" src="<?php echo $external_icons_img; ?>"><span>Flickr</span></a>
-	<a href="#" class="pinterest"><img class="sprite" alt="" src="<?php echo $external_icons_img; ?>"><span>Pinterest</span></a>
-	<a href="#" class="instagram"><img class="sprite" alt="" src="<?php echo $external_icons_img; ?>"><span>Instagram</span></a>
-	<a href="#" class="github"><img class="sprite" alt="" src="<?php echo $external_icons_img; ?>"><span>Github</span></a>
-	<a href="<?php echo get_bloginfo_rss('rss2_url'); ?>" class="rss"><img class="sprite" alt="" src="<?php echo $external_icons_img; ?>"><span>RSS></span></a>
-
+	<!--
+	<a href="#" class="fb">
+		<img class="sprite" alt="" src="<?php echo $external_icons_img; ?>">
+		<span>Facebook</span>
+	</a>
+	-->
+	<a href="#" class="twitter">
+		<img class="sprite" alt="" width="0" height="1" src="<?php echo $external_icons_img; ?>">
+		<span>Twitter</span>
+	</a>
+	<a href="#" class="flickr">
+		<img class="sprite" alt="" width="0" height="1" src="<?php echo $external_icons_img; ?>">
+		<span>Flickr</span>
+	</a>
+	<a href="#" class="pinterest">
+		<img class="sprite" alt="" width="0" height="1" src="<?php echo $external_icons_img; ?>">
+		<span>Pinterest</span>
+	</a>
+	<a href="#" class="instagram">
+		<img class="sprite" alt="" width="0" height="1" src="<?php echo $external_icons_img; ?>">
+		<span>Instagram</span>
+	</a>
+	<a href="#" class="github">
+		<img class="sprite" alt="" width="0" height="1" src="<?php echo $external_icons_img; ?>">
+		<span>Github</span>
+	</a>
+	<a href="<?php echo get_bloginfo_rss('rss2_url'); ?>" class="rss">
+		<img class="sprite" alt="" width="0" height="1" src="<?php echo $external_icons_img; ?>">
+		<span>RSS></span>
+	</a>
 </div>
