2a333cb1692e06d28b414383d757f4f92525dedf
Hannah Hu <hannahlhu@gmail.com>
2014-06-30 18:31:07 -0700

    dealing with external links sprite

--- external-links.php
+++ external-links.php
@@ -1,10 +1,14 @@
+<?php
+	$external_icons_img = get_template_directory_uri() . '/images/external-icons.png';
+?>
+
 <div class="external-links">
-	<a href="#" class="fb"><span>Facebook</span></a>
-	<a href="#" class="twitter"><span>Twitter</span></a>
-	<a href="#" class="flickr"><span>Flickr</span></a>
-	<a href="#" class="pinterest"><span>Pinterest</span></a>
-	<a href="#" class="instagram"><span>Instagram</span></a>
-	<a href="#" class="github"><span>Github</span></a>
-	<a href="<?php get_bloginfo_rss('rss2_url'); ?>" class="rss"><span>RSS></span></a>
+	<a href="#" class="fb"><img class="sprite" alt="" src="<?php echo $external_icons_img; ?>"><span>Facebook</span></a>
+	<a href="#" class="twitter"><img class="sprite" alt="" src="<?php echo $external_icons_img; ?>"><span>Twitter</span></a>
+	<a href="#" class="flickr"><img class="sprite" alt="" src="<?php echo $external_icons_img; ?>"><span>Flickr</span></a>
+	<a href="#" class="pinterest"><img class="sprite" alt="" src="<?php echo $external_icons_img; ?>"><span>Pinterest</span></a>
+	<a href="#" class="instagram"><img class="sprite" alt="" src="<?php echo $external_icons_img; ?>"><span>Instagram</span></a>
+	<a href="#" class="github"><img class="sprite" alt="" src="<?php echo $external_icons_img; ?>"><span>Github</span></a>
+	<a href="<?php echo get_bloginfo_rss('rss2_url'); ?>" class="rss"><img class="sprite" alt="" src="<?php echo $external_icons_img; ?>"><span>RSS></span></a>
 
 </div>
