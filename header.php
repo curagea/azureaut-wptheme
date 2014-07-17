23ed395ee1092aec9156bd2ce8079c50b29aefcf
Hannah Hu <hannahlhu@gmail.com>
2014-07-16 18:19:26 -0700

    favicon!

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
 	<meta charset="<?php bloginfo('charset'); ?>" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name=viewport content="width=device-width, initial-scale=1">
-	<link rel="shortcut icon" href="<?php echo get_template_directory_uri() ?>/images/favicon.ico" />
+	<link rel="shortcut icon" href="<?php echo get_template_directory_uri() ?>/images/favicon.png" />
 	<title>
 		<?php
 			if (is_404()){
